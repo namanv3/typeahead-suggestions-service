8a7fefa9a46f74c952db591d4ddc709125ae9a9e
Naman Verma <naman13verma@gmail.com>
2022-04-08 17:20:03 +0530

    Add top suggestions

---
+++ Beans/suggestion.h
@@ -0,0 +1,45 @@
+#ifndef __SUGGESTION_INCLUDED__
+#define __SUGGESTION_INCLUDED__
+
+#include <string>
+
+using namespace std;
+
+class Suggestion {
+	private:
+		string suggestedWord;
+		int count;
+	public:
+		Suggestion(string word) {
+			suggestedWord = word;
+			count = 0;
+		}
+
+		string getSuggestedWord() {
+			return suggestedWord;
+		}
+
+		int getCount() {
+			return count;
+		}
+
+		void incrementCount() {
+			count++;
+		}
+
+		bool operator == (const Suggestion& s2) {
+			return count == s2.count && suggestedWord == s2.suggestedWord;
+		}
+		bool operator > (const Suggestion& s2) {
+			if (count < s2.count) {
+				return false;
+			} else if (count > s2.count) {
+				return true;
+			}
+
+			return s2.suggestedWord > suggestedWord;
+		}
+};
+
+
+#endif
--- Beans/trieNode.h
+++ Beans/trieNode.h
@@ -1,8 +1,11 @@
 #ifndef __TRIE_NODE_INCLUDED__
 #define __TRIE_NODE_INCLUDED__
 
+#include "suggestion.h"
+
 const int ALPHABET_SIZE = 26;
 const char ROOT_NODE_CHAR = '!';
+const int NUM_SUGGESTIONS = 7;
 
 class TrieNode {
 	private:
@@ -10,13 +13,18 @@ class TrieNode {
 		TrieNode* children[ALPHABET_SIZE];
 		bool rootNode;
 		bool endOfWord;
+		Suggestion* topSuggestions[NUM_SUGGESTIONS];
 	public:
 		TrieNode(char c, bool isEndOfWord) {
 			letter = c;
 			rootNode = false;
 			endOfWord = isEndOfWord;
 			for (int i = 0; i < ALPHABET_SIZE; i++) {
-				children[i] = 0;
+				children[i] = NULL;
+			}
+
+			for (int i = 0; i < NUM_SUGGESTIONS; i++) {
+				topSuggestions[i] = NULL;
 			}
 		}
 
@@ -25,13 +33,20 @@ class TrieNode {
 			rootNode = true;
 			endOfWord = false;
 			for (int i = 0; i < ALPHABET_SIZE; i++) {
-				children[i] = 0;
+				children[i] = NULL;
+			}
+			for (int i = 0; i < NUM_SUGGESTIONS; i++) {
+				topSuggestions[i] = NULL;
 			}
 
 		}
 
 		char getLetter() {
 			return letter;
+		}
+
+		Suggestion** getTopSuggestions() {
+			return topSuggestions;
 		}
 
 		bool isRootNode() {
@@ -52,6 +67,41 @@ class TrieNode {
 
 		void makeEndOfWord() {
 			endOfWord = true;
+		}
+
+		void insertSuggestion(Suggestion* suggestion) {
+			if (suggestion == NULL) {
+				return;
+			}
+
+			int insertAt = NUM_SUGGESTIONS;
+			for (int i = 0; i < NUM_SUGGESTIONS; i++) {
+				if (topSuggestions[i] == NULL || *suggestion > *topSuggestions[i]) {
+					insertAt = i;
+					break;
+				}
+				if (*suggestion == *topSuggestions[i]) {
+					return;
+				}
+			}
+
+			Suggestion* toInsert = suggestion;
+			for (int i = insertAt; i < NUM_SUGGESTIONS; i++) {
+				Suggestion* temp = toInsert;
+				toInsert = topSuggestions[i];
+				topSuggestions[i] = temp;
+			}
+		}
+
+		void updateSuggestionsFromChild(char c) {
+			int i = c - 'a';
+			if (children[i] == 0) {
+				return;
+			}
+			Suggestion** childTopSuggestions = children[i]->getTopSuggestions();
+			for (int j = 0; j < NUM_SUGGESTIONS; j++) {
+				insertSuggestion(childTopSuggestions[j]);
+			}
 		}
 };
 
